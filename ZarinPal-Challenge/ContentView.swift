74ab092db32e8a530b78134f0f8b8d2c994ecad3
Farshad Mousalou <farshadm90@gmail.com>
2020-06-17 01:28:15 +0430

    - created xcode project.

---
+++ ZarinPal-Challenge/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ZarinPal-Challenge
+//
+//  Created by Farshad Mousalou on 6/17/20.
+//  Copyright © 2020 Farshad Mousalou. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
